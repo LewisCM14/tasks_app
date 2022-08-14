92a0815ad4277103096e3d402d93d8b4c2464db2
LewisCM14 <Lewiscmartin@live.co.uk>
2022-08-14 18:38:56 +0100

    configure the useHttp hook

---
+++ src/hooks/use-http.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+/**
+ * Custom hook to handle HTTP requests
+ * requestConfig handles POST or GET requests configuration
+ * Assumes always working with JSON data.
+ * sendRequest checks the response of the request and collects the data,
+ * This data is then passed to the applyData function.
+ *
+ * loading & error state plus sendRequest are returned to the component using the hook
+ */
+
+const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
